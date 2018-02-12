1ac0d864d9e38b180e441a85ca307b35fd795a03
Luke Schoen <ltfschoen@gmail.com>
2018-02-13 08:20:58 +1100

    feat: Add Truffle Config same as DEX repo

--- truffle.js
+++ truffle.js
@@ -1,4 +1,15 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8500,
+      network_id: "3", // Match any network id
+      gas: 7984452, // Block Gas Limit same as latest on Mainnet https://ethstats.net/
+      gasPrice: 2000000000, // same as latest on Mainnet https://ethstats.net/
+      // Mnemonic: "copy obey episode awake damp vacant protect hold wish primary travel shy"
+      from: "0x7c06350cb8640a113a618004a828d3411a4f32d3"
+    }
+  }
 };
